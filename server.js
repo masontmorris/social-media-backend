ac1184d55153b6bc90efdb84042b66927345411f
masontmorris <masonmorris8@gmail.com>
2022-07-23 14:41:59 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+
+const express = require("express");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+const mongoose = require("mongoose");
+
+const { User, Thought } = require("./models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => {
+    console.log(`Backend listening on ${PORT}!`);
+});
